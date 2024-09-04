d9d039829c209eb5baf3b053db40db57e9acc05c
Rukie007 <ruan@ufcons.com>
2024-09-04 15:54:51 +0200

    test

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-# Create a Flask application instance
+# Create a Flask app instance
 application = Flask(__name__)
 
 # Define a route for the root URL ('/')
@@ -8,6 +8,6 @@ application = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-# Run the application if this script is executed directly
+# Run the app if this script is executed directly
 if __name__ == '__main__':
     application.run(debug=True)
